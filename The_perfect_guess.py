d2e8d30d5cfb7c9313a204499bc28b68177d468a
comprehensivegene649837 <92254249+comprehensivegene649837@users.noreply.github.com>
2021-10-19 23:23:07 +0530

    Add files via upload

---
+++ The_perfect_guess.py
@@ -0,0 +1,38 @@
+import random
+randnum =  random.randint(1,100)
+
+userGuess =  None
+guess = 3
+
+while True: #(userGuess!=randnum):
+    userGuess = int(input("enter the number: "))
+    
+    if userGuess!=randnum:
+        guess= guess -1 
+        
+    
+    
+    if userGuess==randnum:
+        print("you guessed it right!")
+    else:    
+        if userGuess>randnum:
+        
+            print("hint: opps! you should have entered a lower number")
+        
+        
+#if userGuess<randnum:
+        else:
+            print("hint: ohh! you were supposed to enter a higher number")
+        
+        
+
+        
+    if guess == 0:
+        #if guess ==1:
+            #print("LAST CHANCE!")
+        
+        print("you ran out of guesses! better luck next time")
+        break
+    print(f"now you have {guess} number of guesses left, be careful!")
+    
+print("right number: ", randnum)
